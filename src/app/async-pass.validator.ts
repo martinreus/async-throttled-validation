e9c0e1092fa226004f8b6b385630c3bd093a7957
Martin Reus <martinreus@gmail.com>
2020-02-10 18:07:10 +0000

    add throttling

--- src/app/async-pass.validator.ts
+++ src/app/async-pass.validator.ts
@@ -5,7 +5,7 @@ import {
   ValidationErrors
 } from '@angular/forms';
 import { Observable, of, Subject, timer } from 'rxjs';
-import { map, switchMap, tap } from 'rxjs/operators';
+import { map, switchMap, tap, timeout } from 'rxjs/operators';
 import { PasswordService } from './password.service';
 
 @Injectable({ providedIn: 'root' })
@@ -15,7 +15,8 @@ export class AsyncPassValidator implements AsyncValidator {
   validate(
     control: AbstractControl
   ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
-    return this.pwService.getPasswordScore(control.value).pipe(
+    return timer(500).pipe(
+      switchMap(_ => this.pwService.getPasswordScore(control.value)),
       map(score => {
         // if password score is below threshold, validation will fail
         if (score < PasswordService.MIN_PASSWORD_SCORE) {
